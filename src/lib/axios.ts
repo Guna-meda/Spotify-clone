70a61b634adf7e67535544dc05dd71e7cc9ef68e
Guna-meda <meda.gunavathi25@gmail.com>
2025-08-10 14:25:54 +0530

    socket url fixed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-baseURL: import.meta.env.VITE_BACKEND_URL
-//baseURL: "http://localhost:3002/api/v1"
+//baseURL: import.meta.env.VITE_BACKEND_URL
+baseURL: "http://localhost:3002/api/v1"
 })
 
